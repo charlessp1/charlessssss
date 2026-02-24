2d9741be4e49ec924df79a52ce0a2657fece0b93
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-02-24 19:31:51 +0800

    upload pls

--- Short  quiz.py
+++
@@ -1,7 +0,0 @@
-first_integer = int(input("Enter an integer: "))
-second_integer = int(input("Enter another integer: "))
-first_integer = first_integer % second_integer
-first_integer = first_integer % second_integer
-second_integer = second_integer % first_integer
-print(second_integer)
-
